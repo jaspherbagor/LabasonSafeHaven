49945cd9452ab81d9a9962111e97d7de2ebcc96d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-17 11:42:31 +0800

    removed the other unnecessary things

--- labasonsafehaven/app/Http/Middleware/Authenticate.php
+++ labasonsafehaven/app/Http/Middleware/Authenticate.php
@@ -8,25 +8,10 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
-    }
-
-    public function handle($request, Closure $next, ...$guards)
-    {
-        if(auth()->user()->role == 2) {
-            return $next($request);
-        }
-
-        return redirect()->route('home');
     }
 }
