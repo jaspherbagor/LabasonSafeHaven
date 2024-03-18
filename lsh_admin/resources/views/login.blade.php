b6aab1e93b220285dff38d63af98efac7c8beec0
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-18 20:39:39 +0800

    updated the margin of the login and registration form

--- lsh_admin/resources/views/login.blade.php
+++ lsh_admin/resources/views/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <div class="login-container container-fluid d-flex align-items-center justify-content-center px-2 py-5">
-    <div class="g-3 needs-validation login-form-container p-3 h-auto">
+    <div class="g-3 needs-validation login-form-container p-3 my-5 h-auto">
         <div class="text-center">
             <a href="{{ route('home') }}">
                 <img src="{{ asset('home_images/logo.png') }}" class="logo-img" alt="logo"/>
