bd4c5a31057bc1ce6e01ec3d1c4bf61c0979ce01
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-20 14:57:57 +0800

    convert the font from FA5->FA4

--- lsh_capstone/resources/views/admin/layout/nav.blade.php
+++ lsh_capstone/resources/views/admin/layout/nav.blade.php
@@ -2,8 +2,8 @@
         <nav class="navbar navbar-expand-lg main-navbar">
             <form class="form-inline mr-auto">
                 <ul class="navbar-nav mr-3">
-                    <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
-                    <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fas fa-search"></i></a></li>
+                    <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fa fa-bars"></i></a></li>
+                    <li><a href="#" data-toggle="search" class="nav-link nav-link-lg d-sm-none"><i class="fa fa-search"></i></a></li>
                 </ul>
             </form>
             <ul class="navbar-nav navbar-right">
@@ -15,10 +15,10 @@
                     <div class="d-sm-none d-lg-inline-block">{{ Auth::guard('admin')->user()->name }}</div></a>
                     <div class="dropdown-menu dropdown-menu-right">
                         <a href="{{ route('admin_profile') }}" class="dropdown-item has-icon">
-                            <i class="far fa-user"></i> Edit Profile
+                            <i class="fa fa-user"></i> Edit Profile
                         </a>
                         <a href="{{ route('admin_logout') }}" class="dropdown-item has-icon text-danger">
-                            <i class="fas fa-sign-out-alt"></i> Logout
+                            <i class="fa fa-sign-out"></i> Logout
                         </a>
                     </div>
                 </li>
--- lsh_capstone/resources/views/admin/slide_view.blade.php
+++ lsh_capstone/resources/views/admin/slide_view.blade.php
@@ -3,7 +3,7 @@
 @section('heading', 'View Slides')
 
 @section('right_top_button')
-<a href="{{ route('admin_slide_add') }}" class="btn btn-primary"><i class="fas fa-plus"></i> Add New</a>
+<a href="{{ route('admin_slide_add') }}" class="btn btn-primary"><i class="fa fa-plus"></i> Add New</a>
 @endsection
 @section('main_content')
     <div class="section-body">
