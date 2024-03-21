a22cc8878e9996369cb556f58f6c6d36a16353d7
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-21 17:05:47 +0800

    added the functionality that can render the testimonial information in the home page

--- lsh_capstone/app/Http/Controllers/Front/HomeController.php
+++ lsh_capstone/app/Http/Controllers/Front/HomeController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\Front;
 use App\Http\Controllers\Controller;
 use App\Models\Feature;
 use App\Models\Slide;
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index()
     {
+        $testimonial_all = Testimonial::get();
         $slide_all = Slide::get();
         $feature_all = Feature::get();
-        return view('front.home',compact('slide_all', 'feature_all'));
+        return view('front.home',compact('slide_all', 'feature_all', 'testimonial_all'));
     }
 }
