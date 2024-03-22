4fa2dadf62c8226c4dcfe46946304964b4774367
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-22 15:07:04 +0800

    added the update and delete video functionality in the admin

--- lsh_capstone/app/Http/Controllers/Admin/AdminVideoController.php
+++ lsh_capstone/app/Http/Controllers/Admin/AdminVideoController.php
@@ -36,43 +36,27 @@ class AdminVideoController extends Controller
 
     public function edit($id)
     {
-        $photo_data = Video::where('id',$id)->first();
-        return view('admin.photo_edit', compact('photo_data'));
+        $video_data = Video::where('id',$id)->first();
+        return view('admin.video_edit', compact('video_data'));
     }
 
     public function update(Request $request, $id)
     {
         $obj = Video::where('id', $id)->first();
 
-        if($request->hasFile('photo')) {
-            $request->validate([
-                'photo' => 'image|mimes:jpeg,jpg,svg,png,webp,gif|max:5120',
-            ]);
-
-            unlink(public_path('uploads/'.$obj->photo));
-
-            $ext = $request->file('photo')->extension();
-            $final_name = time().'.'.$ext;
-
-            $request->file('photo')->move(public_path('uploads/'),$final_name);
-
-
-            $obj->photo = $final_name;           
-        }
-
+        $obj->video_id = $request->video_id;
         $obj->caption = $request->caption;
         $obj->update();
 
-        return redirect()->back()->with('success', 'Photo is updated successfully!');
+        return redirect()->back()->with('success', 'Video is updated successfully!');
         
     }
 
     public function delete($id)
     {
         $single_data = Video::where('id', $id)->first();
-        unlink(public_path('uploads/'.$single_data->photo));
         $single_data->delete();
 
-        return redirect()->back()->with('success', 'Photo is deleted successfully!');
+        return redirect()->back()->with('success', 'Video is deleted successfully!');
     }
 }
