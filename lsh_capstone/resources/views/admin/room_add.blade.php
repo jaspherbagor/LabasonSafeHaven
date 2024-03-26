11464cfee6701c3c0716100e927cd8a2921c9aa3
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-27 06:34:45 +0800

    added the structure of the add room page in the admin panel

--- lsh_capstone/resources/views/admin/room_add.blade.php
+++ lsh_capstone/resources/views/admin/room_add.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.layout.app')
 
-@section('heading', 'Add Slide')
+@section('heading', 'Add Room')
 
 @section('right_top_button')
-<a href="{{ route('admin_slide_view') }}" class="btn btn-primary"><i class="fa fa-eye"></i> View All</a>
+<a href="{{ route('admin_room_view') }}" class="btn btn-primary"><i class="fa fa-eye"></i> View All</a>
 @endsection
 
 @section('main_content')
@@ -12,32 +12,73 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <form action="{{ route('admin_slide_store') }}" method="post" enctype="multipart/form-data">
+                    <form action="{{ route('admin_room_store') }}" method="post" enctype="multipart/form-data">
                         @csrf
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="mb-4">
                                     <label class="form-label">Photo *</label>
                                     <div>
-                                        <input type="file" name="photo">
+                                        <input type="file" name="featured_photo">
                                     </div>
                                 </div>
                                 <div class="mb-4">
-                                    <label class="form-label">Heading *</label>
-                                    <input type="text" class="form-control" name="heading" value={{ old('heading') }}>
+                                    <label class="form-label">Name *</label>
+                                    <input type="text" class="form-control" name="name" value={{ old('name') }}>
                                 </div>
                                 <div class="mb-4">
-                                    <label class="form-label">Text</label>
-                                    <textarea name="text" class="form-control h_100"  cols="30" rows="10">{{ old('text') }}</textarea>
+                                    <label class="form-label">Description *</label>
+                                    <textarea name="description" class="form-control h_100 snote"  cols="30" rows="10">{{ old('description') }}</textarea>
                                 </div>
                                 <div class="mb-4">
-                                    <label class="form-label">Button Text</label>
-                                    <input type="text" class="form-control" name="button_text" value={{ old('button_text') }}>
+                                    <label class="form-label">Price *</label>
+                                    <input type="text" class="form-control" name="price" value={{ old('price') }}>
                                 </div>
                                 <div class="mb-4">
-                                    <label class="form-label">Button URL</label>
-                                    <input type="text" class="form-control" name="button_url" value={{ old('button_url') }}>
+                                    <label class="form-label">Total Rooms *</label>
+                                    <input type="text" class="form-control" name="total_rooms" value={{ old('total_rooms') }}>
                                 </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Amenities</label>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" name="amenities">
+                                        <label class="form-check-label" for="defaultCheck1">
+                                          Amenity 1
+                                        </label>
+                                      </div>
+                                      <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" value="" id="defaultCheck2" name="amenities">
+                                        <label class="form-check-label" for="defaultCheck2">
+                                          Amenity 2
+                                        </label>
+                                      </div>
+                                    
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Room Size</label>
+                                    <input type="text" class="form-control" name="size" value={{ old('size') }}>
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Beds </label>
+                                    <input type="text" class="form-control" name="total_beds" value={{ old('total_beds') }}>
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Bathrooms </label>
+                                    <input type="text" class="form-control" name="total_bathrooms" value={{ old('total_bathrooms') }}>
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Balconies </label>
+                                    <input type="text" class="form-control" name="total_balconies" value={{ old('total_balconies') }}>
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Guests </label>
+                                    <input type="text" class="form-control" name="total_guests" value={{ old('total_guests') }}>
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Video Id </label>
+                                    <input type="text" class="form-control" name="video_id" value={{ old('video_id') }}>
+                                </div>
+                                
                                 <div class="mb-4">
                                     <label class="form-label"></label>
                                     <button type="submit" class="btn btn-primary">Submit</button>
