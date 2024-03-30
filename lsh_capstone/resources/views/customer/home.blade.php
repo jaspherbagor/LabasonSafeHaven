0b1c1390cd22ad72167d08aff3e623251dc2bdfd
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-03-30 13:05:58 +0800

    updated the customer home page

--- lsh_capstone/resources/views/customer/home.blade.php
+++ lsh_capstone/resources/views/customer/home.blade.php
@@ -14,7 +14,7 @@
                     <h4>Completed Orders</h4>
                 </div>
                 <div class="card-body">
-                    8
+                    {{ $total_completed_orders }}
                 </div>
             </div>
         </div>
@@ -22,14 +22,14 @@
     <div class="col-lg-4 col-md-6 col-sm-6 col-12">
         <div class="card card-statistic-1">
             <div class="card-icon bg-danger">
-                <i class="fa fa-book-open"></i>
+                <i class="fa fa-book"></i>
             </div>
             <div class="card-wrap">
                 <div class="card-header">
                     <h4>Pending Orders</h4>
                 </div>
                 <div class="card-body">
-                    15
+                    {{ $total_pending_orders }}
                 </div>
             </div>
         </div>
