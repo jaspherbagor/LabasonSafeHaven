4d8f756dcad5d5558215e48e962a5f2f7a69b92c
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-09 19:12:46 +0800

    added comments on each lines of the codes in the blog controller

--- lsh_capstone/app/Http/Controllers/Front/BlogController.php
+++ lsh_capstone/app/Http/Controllers/Front/BlogController.php
@@ -1,24 +1,32 @@
 <?php
 
-namespace App\Http\Controllers\Front;
+namespace App\Http\Controllers\Front; // Namespace declaration
 
-use App\Http\Controllers\Controller;
-use App\Models\Post;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller; // Import the base controller class
+use App\Models\Post; // Import the Post model
+use Illuminate\Http\Request; // Import the Request class
 
-class BlogController extends Controller
+class BlogController extends Controller // Define the BlogController class, extending the base controller
 {
-    public function index()
+    public function index() // Define the index method to display the blog page
     {
-        $post_all = Post::orderBy('id', 'desc')->paginate(9);
-        return view('front.blog',compact('post_all'));
+        // Retrieve all posts from the database, ordered by ID in descending order and paginated
+        $post_all = Post::orderBy('id', 'desc')->paginate(9); 
+        
+        // Return the view for the blog page, passing the retrieved posts to the view
+        return view('front.blog', compact('post_all')); 
     }
 
-    public function single_post($id)
+    public function single_post($id) // Define the single_post method to display a single post
     {
-        $single_post_data = Post::where('id', $id)->first();
-        $single_post_data->total_view = $single_post_data->total_view+1;
-        $single_post_data->update();
-        return view('front.post',compact('single_post_data'));
+        // Retrieve the data of the single post from the database
+        $single_post_data = Post::where('id', $id)->first(); 
+        
+        // Increment the total view count of the single post
+        $single_post_data->total_view = $single_post_data->total_view + 1;
+        $single_post_data->update(); // Update the post data in the database
+        
+        // Return the view for the single post, passing the retrieved single post data to the view
+        return view('front.post', compact('single_post_data')); 
     }
 }
