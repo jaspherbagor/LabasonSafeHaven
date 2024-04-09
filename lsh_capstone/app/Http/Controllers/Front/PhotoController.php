f430cf95b1f1df9053d46ed8f7c4d22684a417a0
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-09 19:25:10 +0800

    added comments on the lines of the codes in the photo controller

--- lsh_capstone/app/Http/Controllers/Front/PhotoController.php
+++ lsh_capstone/app/Http/Controllers/Front/PhotoController.php
@@ -8,9 +8,17 @@ use Illuminate\Http\Request;
 
 class PhotoController extends Controller
 {
+    /**
+     * Display a listing of the photos.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
+        // Retrieve paginated photos
         $photo_all = Photo::paginate(12);
-        return view('front.photo_gallery',compact('photo_all'));
+
+        // Return the view with photo data
+        return view('front.photo_gallery', compact('photo_all'));
     }
 }
