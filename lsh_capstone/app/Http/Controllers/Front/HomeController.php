1afc9d325a2455439c4df0e83391ed53f27e72a2
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-09 19:23:53 +0800

    added comments in the lines of the home controller

--- lsh_capstone/app/Http/Controllers/Front/HomeController.php
+++ lsh_capstone/app/Http/Controllers/Front/HomeController.php
@@ -9,15 +9,32 @@ use App\Models\Room;
 use App\Models\Slide;
 use App\Models\Testimonial;
 use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
+    /**
+     * Display the home page.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
+        // Retrieve all testimonials
         $testimonial_all = Testimonial::get();
+
+        // Retrieve all slides
         $slide_all = Slide::get();
+
+        // Retrieve all features
         $feature_all = Feature::get();
+
+        // Retrieve the latest 3 posts
         $post_all = Post::orderBy('id', 'desc')->limit(3)->get();
+
+        // Retrieve all rooms
         $room_all = Room::get();
-        return view('front.home',compact('slide_all', 'feature_all', 'testimonial_all', 'post_all', 'room_all'));
+
+        // Return the view with data
+        return view('front.home', compact('slide_all', 'feature_all', 'testimonial_all', 'post_all', 'room_all'));
     }
 }
