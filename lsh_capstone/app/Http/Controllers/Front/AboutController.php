732421f2a9cce710c0e12a7ee4f7326ba9c5ceba
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-09 19:11:23 +0800

    added comments to the lines of the codes in the about controller

--- lsh_capstone/app/Http/Controllers/Front/AboutController.php
+++ lsh_capstone/app/Http/Controllers/Front/AboutController.php
@@ -1,16 +1,19 @@
 <?php
 
-namespace App\Http\Controllers\Front;
+namespace App\Http\Controllers\Front; // Namespace declaration
 
-use App\Http\Controllers\Controller;
-use App\Models\Page;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller; // Import the base controller class
+use App\Models\Page; // Import the Page model
+use Illuminate\Http\Request; // Import the Request class
 
-class AboutController extends Controller
+class AboutController extends Controller // Define the AboutController class, extending the base controller
 {
-    public function index()
+    public function index() // Define the index method to display the about page
     {
-        $about_data = Page::where('id', 1)->first();
-        return view('front.about', compact('about_data'));
+        // Retrieve the about page data from the database
+        $about_data = Page::where('id', 1)->first(); 
+        
+        // Return the view for the about page, passing the about page data to the view
+        return view('front.about', compact('about_data')); 
     }
 }
