2c78356b6b483cfe626001fd8ef370ca1affa83d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-09 19:27:50 +0800

    added comments on each lines of the codes in the room controller

--- lsh_capstone/app/Http/Controllers/Front/RoomController.php
+++ lsh_capstone/app/Http/Controllers/Front/RoomController.php
@@ -8,15 +8,32 @@ use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
+    /**
+     * Display a paginated list of rooms.
+     *
+     * @return \Illuminate\View\View
+     */
     public function index()
     {
+        // Retrieve paginated list of rooms
         $room_all = Room::paginate(12);
+        
+        // Return the view with room data
         return view('front.room', compact('room_all'));
     }
 
+    /**
+     * Display the details of a single room.
+     *
+     * @param  int  $id
+     * @return \Illuminate\View\View
+     */
     public function single_room($id)
     {
-        $single_room_data = Room::with('rRoomPhoto')->where('id',$id)->first();
+        // Retrieve the details of a single room with its photos
+        $single_room_data = Room::with('rRoomPhoto')->where('id', $id)->first();
+        
+        // Return the view with single room data
         return view('front.room_detail', compact('single_room_data'));
     }
 }
