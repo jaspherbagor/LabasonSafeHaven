be5b7bd7c1ad6d6e2137cacf7df24005f325d51d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-10 17:19:15 +0800

    added comments on the lines of the codes in the customer home controller

--- lsh_capstone/app/Http/Controllers/Customer/CustomerHomeController.php
+++ lsh_capstone/app/Http/Controllers/Customer/CustomerHomeController.php
@@ -9,11 +9,16 @@ use Illuminate\Support\Facades\Auth;
 
 class CustomerHomeController extends Controller
 {
+    // Display the customer dashboard
     public function index()
     {
+        // Retrieve the total completed orders for the logged-in customer
         $total_completed_orders = Order::where('status','Completed')->where('customer_id',Auth::guard('customer')->user()->id)->count();
+        
+        // Retrieve the total pending orders for the logged-in customer
         $total_pending_orders = Order::where('status','Pending')->where('customer_id',Auth::guard('customer')->user()->id)->count();
+        
+        // Pass the total completed and pending orders to the view
         return view('customer.home', compact('total_completed_orders','total_pending_orders'));
-
     }
 }
