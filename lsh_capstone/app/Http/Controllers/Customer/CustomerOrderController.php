c1299c2c0ba23f540f3c8f170301d03ece90178d
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-10 17:20:27 +0800

    added comments in the customer order controller

--- lsh_capstone/app/Http/Controllers/Customer/CustomerOrderController.php
+++ lsh_capstone/app/Http/Controllers/Customer/CustomerOrderController.php
@@ -11,16 +11,24 @@ use App\Models\OrderDetail;
 
 class CustomerOrderController extends Controller
 {
+    // Display a list of orders for the logged-in customer
     public function index()
     {
-        $orders = Order::where('customer_id',Auth::guard('customer')->user()->id)->get();
+        // Retrieve orders associated with the logged-in customer
+        $orders = Order::where('customer_id', Auth::guard('customer')->user()->id)->get();
+        
+        // Pass the retrieved orders to the view
         return view('customer.orders', compact('orders'));
     }
 
+    // Display invoice for a specific order
     public function invoice($id)
     {
+        // Retrieve the order details for the specified order ID
         $order = Order::where('id', $id)->first();
-        $order_detail = OrderDetail::where('order_id',$id )->get();
+        $order_detail = OrderDetail::where('order_id', $id)->get();
+        
+        // Pass the order and its details to the view
         return view('customer.invoice', compact('order', 'order_detail'));
     }
 }
