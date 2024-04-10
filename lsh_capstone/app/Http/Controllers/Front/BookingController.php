4b8d8b16ec058c2ff8952d9b1322348feafc6eae
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-10 16:44:45 +0800

    added comments to the cart view and cart delete function in the booking controller

--- lsh_capstone/app/Http/Controllers/Front/BookingController.php
+++ lsh_capstone/app/Http/Controllers/Front/BookingController.php
@@ -79,73 +79,81 @@ class BookingController extends Controller
 }
 
 
-    public function cart_view()
-    {
-        return view('front.cart');
+public function cart_view()
+{
+    // Render the view for the cart page
+    return view('front.cart');
+}
+
+public function cart_delete($id)
+{
+    // Create arrays to store cart session data
+    $arr_cart_room_id = array();
+    $i = 0;
+    
+    // Copy cart_room_id session data to an array
+    foreach (session()->get('cart_room_id') as $value) {
+        $arr_cart_room_id[$i] = $value;
+        $i++;
     }
 
-    public function cart_delete($id)
-    {
-        $arr_cart_room_id = array();
-        $i=0;
-        foreach(session()->get('cart_room_id') as $value) {
-            $arr_cart_room_id[$i] = $value;
-            $i++;
-        }
-
-        $arr_cart_checkin_date = array();
-        $i=0;
-        foreach(session()->get('cart_checkin_date') as $value) {
-            $arr_cart_checkin_date[$i] = $value;
-            $i++;
-        }
-
-        $arr_cart_checkout_date = array();
-        $i=0;
-        foreach(session()->get('cart_checkout_date') as $value) {
-            $arr_cart_checkout_date[$i] = $value;
-            $i++;
-        }
-
-        $arr_cart_adult = array();
-        $i=0;
-        foreach(session()->get('cart_adult') as $value) {
-            $arr_cart_adult[$i] = $value;
-            $i++;
-        }
-
-        $arr_cart_children = array();
-        $i=0;
-        foreach(session()->get('cart_children') as $value) {
-            $arr_cart_children[$i] = $value;
-            $i++;
-        }
-
-        session()->forget('cart_room_id');
-        session()->forget('cart_checkin_date');
-        session()->forget('cart_checkout_date');
-        session()->forget('cart_adult');
-        session()->forget('cart_children');
-
-        for($i=0;$i<count($arr_cart_room_id);$i++)
-        {
-            if($arr_cart_room_id[$i] == $id) 
-            {
-                continue;    
-            }
-            else
-            {
-                session()->push('cart_room_id',$arr_cart_room_id[$i]);
-                session()->push('cart_checkin_date',$arr_cart_checkin_date[$i]);
-                session()->push('cart_checkout_date',$arr_cart_checkout_date[$i]);
-                session()->push('cart_adult',$arr_cart_adult[$i]);
-                session()->push('cart_children',$arr_cart_children[$i]);
-            }
-        }
-
-        return redirect()->back()->with('success', 'Cart item is deleted.');
-
+    // Similar process for other cart session data
+    // Copy cart_checkin_date session data to an array
+    $arr_cart_checkin_date = array();
+    $i = 0;
+    foreach (session()->get('cart_checkin_date') as $value) {
+        $arr_cart_checkin_date[$i] = $value;
+        $i++;
     }
+
+    // Copy cart_checkout_date session data to an array
+    $arr_cart_checkout_date = array();
+    $i = 0;
+    foreach (session()->get('cart_checkout_date') as $value) {
+        $arr_cart_checkout_date[$i] = $value;
+        $i++;
+    }
+
+    // Copy cart_adult session data to an array
+    $arr_cart_adult = array();
+    $i = 0;
+    foreach (session()->get('cart_adult') as $value) {
+        $arr_cart_adult[$i] = $value;
+        $i++;
+    }
+
+    // Copy cart_children session data to an array
+    $arr_cart_children = array();
+    $i = 0;
+    foreach (session()->get('cart_children') as $value) {
+        $arr_cart_children[$i] = $value;
+        $i++;
+    }
+
+    // Clear the cart session data
+    session()->forget('cart_room_id');
+    session()->forget('cart_checkin_date');
+    session()->forget('cart_checkout_date');
+    session()->forget('cart_adult');
+    session()->forget('cart_children');
+
+    // Re-add cart session data except for the deleted item
+    for ($i = 0; $i < count($arr_cart_room_id); $i++) {
+        if ($arr_cart_room_id[$i] == $id) {
+            continue;
+        } else {
+            session()->push('cart_room_id', $arr_cart_room_id[$i]);
+            session()->push('cart_checkin_date', $arr_cart_checkin_date[$i]);
+            session()->push('cart_checkout_date', $arr_cart_checkout_date[$i]);
+            session()->push('cart_adult', $arr_cart_adult[$i]);
+            session()->push('cart_children', $arr_cart_children[$i]);
+        }
+    }
+
+    // Redirect back to the cart page with success message
+    return redirect()->back()->with('success', 'Cart item is deleted.');
+}
+
 
 
     public function checkout()
